86fb7bf74aafa629dae11f3ff241c9d0f97e4f52
chapito96 <satorres1@uc.cl>
2021-05-03 00:29:33 -0400

    django heroku move to last in settings

--- tarea/tarea/settings.py
+++ tarea/tarea/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-django_heroku.settings(locals())
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,3 +128,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
